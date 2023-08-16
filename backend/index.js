adca715254ea3f6baee0ca06128558920c275bfc
CoolAir <rusmanoctavio@gmail.com>
2023-08-16 09:02:36 -0300

    Se instalo express y mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,11 @@
-import { Application, Sprite, Assets } from 'pixi.js';
+//Create a Pixi Application
+const app = new PIXI.Application({width: 256, height: 256});
+//Rendering parameters
+app.renderer.backgroundColor = 0x061639;
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoDensity = true;
+app.resizeTo = window;
 
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
-const app = new Application();
-
-// The application will create a canvas element for you that you
-// can then insert into the DOM
+//Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
-
-// load the texture we need
-const texture = await Assets.load('bunny.png');
-
-// This creates a texture from a 'bunny.png' image
-const bunny = new Sprite(texture);
-
-// Setup the position of the bunny
-bunny.x = app.renderer.width / 2;
-bunny.y = app.renderer.height / 2;
-
-// Rotate around the center
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
-
-// Add the bunny to the scene we are building
-app.stage.addChild(bunny);
-
-// Listen for frame updates
-app.ticker.add(() => {
-    // each frame we spin the bunny around a bit
-    bunny.rotation += 0.01;
-});
