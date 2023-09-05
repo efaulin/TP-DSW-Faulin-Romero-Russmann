7d1ae07ef0c422f995346d1cd003b3baca8c2c50
Eugenio Faulin <pandufaulin@gmail.com>
2023-09-04 22:54:57 -0300

    Instalacion de modulos JWT Token Instalacion de modulos para la implementacion de un sistemas de tokens 	- jsonwebtoken 	- dotenv 	- bcryptjs

--- backend/backend.js
+++ backend/backend.js
@@ -14,22 +14,22 @@ server.listen(3000, () => {
 
 const port = 27015;
 const express = require('express');
-const app = express();
 const http = require('http');
-const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, {pingInterval: 2000, pingTimeout: 5000});
-const path = require('path');
-
-function logger(req, res, next){
-  console.log(`Peticion de Acceso -> ${req.protocol}://${req.get('host')}${req.originalUrl}`)
-  next()
-}
+const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname))
 app.use(logger);
+
+const server = http.createServer(app);
+const io = new Server(server, {pingInterval: 2000, pingTimeout: 5000});
+
+function logger(req, res, next) {
+    console.log(`Peticion de Acceso -> ${req.protocol}://${req.get('host')}${req.originalUrl}`)
+    next()
+}
 
 //#region Pruebas
 app.get('/', (req, res) => {
@@ -73,10 +73,23 @@ const player = {}
 
 app.get('/test', (req, res) => {
   console.log(`LogIn: ` + req.body.user);
-  res.sendFile('frontend.html', {
-    root: __dirname
-  })
+  res.sendFile('frontend.html', { root: __dirname })
 })
+
+app.get('/play', (req, res) => {
+    console.log(`LogIn: ` + req.body.user);
+    res.sendFile('match.html', { root: __dirname + '/../frontend/'})
+})
+
+app.get('/entity/player', (req, res) => {
+    res.sendFile('player.js', { root: __dirname + '/entity/' })
+});
+
+app.get('/script/match', (req, res) => {
+    res.sendFile('match.js', { root: __dirname + '/script/' })
+});
+
+
 
 io.on('connection', (socket) => {
   var usr = socket.handshake.headers.user;
@@ -87,12 +100,6 @@ io.on('connection', (socket) => {
     x: 0,
     y: 0
   }
-
-  socket.on('changeUser', (arg) => {
-    player[socket.id].user = arg
-    console.log(`user changed -> ${player[socket.id].user}`)
-    io.emit('updatePlayers', player)
-  })
 
   socket.on('disconnect', (reason) => {
     delete player[socket.id]
---
+++ backend/script/match.js
@@ -0,0 +1,52 @@
+const urlParams = new URLSearchParams(window.location.search);
+const user = urlParams.get('user');
+var socket = io({
+    extraHeaders: {
+        "user": user
+    }
+});
+const players = {}
+
+socket.on('updatePlayers', (backendPlayers) => {
+    for (const id in backendPlayers) {
+        const bkdPly = backendPlayers[id]
+
+        if (!players[id]) {
+            players[id] = new Player({
+                user: bkdPly.user,
+                x: bkdPly.x,
+                y: bkdPly.y,
+                label: document.createElement("label"),
+                salto: document.createElement("br")
+            });
+
+            var lista = document.getElementById("lobby");
+            lista.appendChild(players[id].label);
+            lista.appendChild(players[id].salto);
+        }
+    }
+
+    for (const id in players) {
+        if (!backendPlayers[id]) {
+            players[id].label.remove()
+            players[id].salto.remove()
+            delete players[id]
+        }
+    }
+
+    console.log(players)
+});
+
+socket.on('newMsg', (msg) => {
+    var li = document.createElement("li");
+    li.textContent = `[${msg.user}]: ${msg.msg}`;
+    var chat = document.getElementById("chat");
+    chat.appendChild(li);
+})
+
+function sendMsg() {
+    var text = document.getElementById("text");
+    console.log(text.value)
+    socket.emit("sendMsg", text.value);
+    text.value = "";
+}
