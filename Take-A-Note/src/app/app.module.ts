80fa8bc50491ec89232c13386ad9273ee417c046
Alan <alanromero2773@gmail.com>
2023-10-26 19:39:25 -0300

    Se agregaron componentes y se dio formato a home y login

--- Take-A-Note/src/app/app.module.ts
+++ Take-A-Note/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RecoveryComponent } from './recovery/recovery.component';
+import { PasswordChangeComponent } from './password-change/password-change.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    PasswordChangeComponent
   ],
   imports: [
     BrowserModule,
