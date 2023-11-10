9fcc879d3b617b0f970cb23fe22703208a568fa4
Octavio <rusmanoctavio@gmail.com>
2023-11-10 12:35:47 -0300

    se agrega la creacion y busqueda de tablas, se tiene que resolver la consulta de notas por tabla que no funciona

--- Take-A-Note/src/app/boardnotes/boardnotes.component.ts
+++ Take-A-Note/src/app/boardnotes/boardnotes.component.ts
@@ -19,10 +19,12 @@ export class BoardnotesComponent implements OnInit {
   ngOnInit(): void {
     this.boardnotes.idboard.subscribe((id) => {
       this.rest
-        .get('http://localhost:3000/api/tablas/' + id)
+        .get(`http://localhost:3000/api/notas/buscar/${id}`)
         .subscribe((res) => {
-          this.notes = Object.values(res);
-        });
+          console.log(JSON.parse(JSON.stringify(res)));
+          this.notes = JSON.parse(JSON.stringify(res))});
+        console.log(id);
+        //console.log(this.notes);
     });
   }
 }
